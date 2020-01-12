7368fb5107e9796d277e239029d64b6025d884b5
Oleg Andreyev <oleg@andreyev.lv>
2020-01-12 02:17:59 +0200

    updated .travis.yml and before_script.sh

--- .build/before_script.sh
+++ .build/before_script.sh
@@ -45,7 +45,7 @@ if [[ "$BROWSER_NAME" = "chrome" ]]; then
     fi;
 
     wget -q -t 3 "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_${machine}.zip" -O driver.zip;
-    unzip -f driver.zip -d chromedriver;
+    unzip -f driver.zip -d chromedriver/;
 fi
 
 if [[ "$BROWSER_NAME" = "firefox" && "$GECKODRIVER_VERSION" = "latest" ]]; then
@@ -64,7 +64,7 @@ if [[ "$BROWSER_NAME" = "firefox" ]]; then
     fi;
 
     wget -q -t 3 "https://github.com/mozilla/geckodriver/releases/download/${GECKODRIVER_VERSION}/geckodriver-$GECKODRIVER_VERSION-${machine}.tar.gz" -O driver.tar.gz;
-    tar -xf driver.tar.gz -C ./geckodriver;
+    tar -xf driver.tar.gz -C ./geckodriver/;
 fi
 
 
