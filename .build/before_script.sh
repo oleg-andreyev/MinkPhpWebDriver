148487a32976440036fe966407da0165c6ac5db4
Oleg Andreyev <oleg@andreyev.lv>
2020-01-13 00:20:20 +0200

    7.4 travis

--- .build/before_script.sh
+++ .build/before_script.sh
@@ -9,6 +9,16 @@ sed -ir 's/self::/static::/g' vendor/symfony/phpunit-bridge/Legacy/SetUpTearDown
 mkdir -p ./logs
 rm -rf ./logs/*
 
+# removing xdebug
+phpenv config-rm xdebug.ini
+
+if [[ $TRAVIS_PHP_VERSION = "7.4"* ]]; then
+    # installing missing zip for 7.4
+    pecl install zip
+    echo "extension=zip.so" > ./ext-zip.ini
+    phpenv config-add ./ext-zip.ini
+fi;
+
 case "$(uname -s)" in
     Linux*)     machine=linux;;
     Darwin*)    machine=mac;;
