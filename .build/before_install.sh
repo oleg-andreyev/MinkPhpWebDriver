79588bd5186cd9336532e1357debe2423a7e937a
Oleg Andreyev <oleg@andreyev.lv>
2020-01-13 01:00:16 +0200

    debug before_install.sh

--- .build/before_install.sh
+++ .build/before_install.sh
@@ -7,7 +7,7 @@ phpenv config-rm xdebug.ini
 
 if [[ $TRAVIS_PHP_VERSION = "7.4"* ]]; then
     sudo apt update
-    sudo apt search libzip-dev
+    sudo apt install libzip-dev
     # installing missing zip for 7.4
     pecl install zip
     echo "extension=zip.so" > ./ext-zip.ini
