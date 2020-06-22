1d35854343fdad53cf83e8f0d39e4b8d62afc9b6
Oleg Andreyev <oleg@andreyev.lv>
2020-06-23 00:46:27 +0300

    added 7.4 + workaround for xenial

--- .build/before_install.sh
+++ .build/before_install.sh
@@ -8,9 +8,16 @@ phpenv config-rm xdebug.ini
 if [[ $TRAVIS_PHP_VERSION = "7.4"* ]]; then
     sudo apt update
     # https://github.com/php/php-src/blob/00821b807ce7953fd6a831f9a28ec7a2fcb1ce36/ext/zip/config.m4#L16
-    sudo apt install -y unzip libssl-dev libzip-dev
-    # installing missing zip for 7.4
-    pecl install zip
-    echo "extension=zip.so" > ./ext-zip.ini
-    phpenv config-add ./ext-zip.ini
+    # xenial ships with libzip 1.0.1 https://launchpad.net/ubuntu/xenial/+source/libzip
+    # xenial ships with libssl 1.0.2g https://launchpad.net/ubuntu/xenial/+source/openssl
+    wget https://launchpad.net/ubuntu/+archive/primary/+files/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+    wget https://launchpad.net/ubuntu/+archive/primary/+files/libssl-dev_1.1.0g-2ubuntu4_amd64.deb
+    wget https://launchpad.net/ubuntu/+archive/primary/+files/libzip5_1.5.1-0ubuntu1_amd64.deb
+    wget https://launchpad.net/ubuntu/+archive/primary/+files/libzip-dev_1.5.1-0ubuntu1_amd64.deb
+    sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+    sudo dpkg -i libssl-dev_1.1.0g-2ubuntu4_amd64.deb
+    sudo dpkg -i libzip5_1.5.1-0ubuntu1_amd64.deb
+    sudo dpkg -i libzip-dev_1.5.1-0ubuntu1_amd64.deb
+    sudo apt install -f
+    sudo apt install -y unzip
 fi;
