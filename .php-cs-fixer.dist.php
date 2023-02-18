6d6c755c6b7fe7d410133e8f15f6774fb4ade80b
Oleg Andreyev <oleg@andreyev.lv>
2023-02-18 18:01:14 +0200

    updated ci

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder);
